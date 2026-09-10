19a8b6ac80a5aed7d9c0aebbbf49824228255cc2
Ser_Kar <ivkarsakov79@gmail.com>
2026-09-10 23:30:07 +0700

    can copy class name

--- script/script.js
+++ script/script.js
@@ -1,9 +1,24 @@
 const elementCodes = document.querySelectorAll('.element-code');
 
 elementCodes.forEach(elementCode => {
-    elementCode.addEventListener('click', () => {
+    elementCode.addEventListener('click', async () => {
         const element = elementCode.closest('.element');
         const button = element.querySelector('.btn');
-        console.log(button.className);
+
+        if (!button) return;
+
+        try {
+            const codeText = button.className;
+            await navigator.clipboard.writeText(codeText);
+
+            const originalHTML = elementCode.innerHTML;
+            elementCode.innerHTML = '<code class="codes">Скопировано</code>';
+            setTimeout(() => {
+                elementCode.innerHTML = originalHTML;
+            }, 1500);
+        }
+        catch (err) {
+            console.error("Ошибка копирования", err);
+        }
     });
 });
